9335a86c05172774b6b040279266be45066e7660
pjb0811 <pjb0811@gmail.com>
2025-09-07 19:42:58 +0900

    ✨ feat: 기본 애플리케이션 구조 및 컴포넌트 통합
    - 새로운 index.tsx 파일 생성
    - Context, Dnd, Editor, Error, Preview 컴포넌트 통합
    - LiveProvider로서 App 컴포넌트 정의 및 관련 컴포넌트 내보내기

---
+++ src/index.tsx
@@ -0,0 +1,27 @@
+import './globals.css';
+
+import Context from './components/Context';
+import LiveDnd from './components/Dnd';
+import LiveEditor from './components/Editor';
+import LiveError from './components/Error';
+import LivePreview from './components/Preview';
+
+const App = ({ children }: { children?: React.ReactNode }) => {
+  return <Context>{children}</Context>;
+};
+
+export {
+  LivePreview,
+  LiveError,
+  LiveEditor,
+  LiveDnd,
+  App as LiveProvider,
+  //
+};
+
+App.Preview = LivePreview;
+App.Error = LiveError;
+App.Editor = LiveEditor;
+App.Dnd = LiveDnd;
+
+export default App;
