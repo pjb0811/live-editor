206cb7d4e77e1068c21cf648f430b556637fccba
pjb0811 <pjb0811@gmail.com>
2025-09-07 19:42:17 +0900

    ✨ feat: ESLint 및 Prettier 설정 업데이트
    - ESLint 구성 파일에서 플러그인 및 규칙 추가
    - Prettier 설정 파일에서 모듈 경로 패턴 수정
    - package.json 및 package-lock.json에 새로운 의존성 추가
    - tsconfig.app.json에 baseUrl 및 paths 설정 추가

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,12 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { globalIgnores } from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', '.history']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
@@ -19,5 +19,23 @@ export default tseslint.config([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
+      'react-refresh/only-export-components': 'warn',
+    },
   },
-])
+]);
