4247332792b2d44281b658521ec65f1c841fbb32
pjb0811 <pjb0811@gmail.com>
2025-12-21 19:23:26 +0900

    ✨ feat: ContextProvider 컴포넌트 개선 및 캐시 관리 추가
    - 기본 코드 상태를 DEFAULT_TEMPLATE으로 설정
    - 컴포넌트 언마운트 시 clearCompilationCache 함수 호출을 위한 useEffect 추가
    - 코드 가독성 향상

--- src/components/Context/index.tsx
+++ src/components/Context/index.tsx
@@ -1,13 +1,22 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+
+import { DEFAULT_TEMPLATE } from '~/enums';
+import { clearCompilationCache } from '~/utils';
 
 import type { ErrorContextType } from './states';
 import { ErrorContext, PreviewContext } from './states';
 
 const ContextProvider = ({ children }: { children?: React.ReactNode }) => {
-  const [code, setCode] = useState('');
+  const [code, setCode] = useState(DEFAULT_TEMPLATE);
   const [error, setError] = useState<ErrorContextType['error']>(null);
+
+  useEffect(() => {
+    return () => {
+      clearCompilationCache();
+    };
+  }, []);
 
   return (
     <PreviewContext.Provider value={{ code, setCode }}>
