3dd2f7073ceecb16a9a853d98966a6609d933063
pjb0811 <pjb0811@gmail.com>
2025-12-23 09:28:53 +0900

    ✨ feat: Renderer 컴포넌트 최적화 및 Props 인터페이스 개선
    - useMemo 훅을 활용하여 성능 최적화 및 코드 중복 제거
    - Props 인터페이스에 headers 및 modules 속성 추가로 타입 안정성 향상
    - useElementSize 훅의 반환값 변수명을 변경하여 가독성 개선

--- src/components/Dnd/Renderer/index.tsx
+++ src/components/Dnd/Renderer/index.tsx
@@ -1,6 +1,15 @@
+import { useMemo } from 'react';
+
 import { useElementSize } from 'use-hooks';
 
-import { baseModules, compileModule, generateSection } from '~/utils';
+import { baseModules, compile, generateSection } from '~/utils';
+
+interface Props {
+  fullCode: string;
+  code: string;
+  modules?: Record<string, unknown>;
+  headers?: Record<string, boolean>;
+}
 
 interface Props {
   fullCode: string;
@@ -10,18 +19,35 @@ interface Props {
 }
 
 const Renderer = ({ fullCode, code, headers, modules }: Props) => {
-  const { breakpoint, elementRef } = useElementSize<HTMLDivElement>();
-  const module = compileModule(generateSection(code, fullCode), {
-    ...baseModules,
-    ...modules,
-  });
-  const Component = module.exports.default || (() => null);
+  const { breakpoint, ref } = useElementSize<HTMLDivElement>();
+
+  const memoizedModules = useMemo(
+    () => ({
+      ...baseModules,
+      ...modules,
+    }),
+    [modules],
+  );
+
+  const memoizedSection = useMemo(
+    () => generateSection(code, fullCode),
+    [code, fullCode],
+  );
+
+  const module = useMemo(
+    () => compile(memoizedSection, memoizedModules),
+    [memoizedSection, memoizedModules],
+  );
+
+  const Component = useMemo(
+    () => module.exports.default || (() => null),
+    [module.exports.default],
+  );
 
   return (
-    <>
-      <div ref={elementRef} className="w-full" />
+    <div ref={ref} className="w-full overflow-x-hidden">
       <Component breakpoint={breakpoint} headers={headers} />
-    </>
+    </div>
   );
 };
 
